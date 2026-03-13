d5358a7b90f73f978121957dfc5e03e0440edfeb
Darya-Niaki <120230954+Darya-Niaki@users.noreply.github.com>
2026-03-13 15:40:26 -0400

    Update CRM_for test.R

--- CRM_for test.R
+++ CRM_for test.R
@@ -1,7 +1,7 @@
 # This for CRM
 library(dfcrm)
 # Target 0.3
-
+# Sceanrios used for paper
 target<-0.3
 r1<-c(0.3,	0.36,	0.42,	0.45,	0.46)
 r2<-c(0.3,	0.4,	0.55,	0.6,	0.7)
@@ -16,17 +16,14 @@ r10<-c(0.06, 0.07, 0.08, 0.12, 0.3)
 
 paper_scenarios_0.3 = rbind(r1,r2,r3,r4,r5,r6,r7,r8,r9,r10)
 
-nsim <- 100   #number of simulated trials
+nsim <- 10000   #number of simulated trials
 target <- 0.30	#target DLT rate
 n.stop <- 31	#number of pts to accrue on one dose level to stop
 n <- 30		#total sample size
 cl <- 0.95		#probability cutoff that defines the safety stopping rule
 
-# try method = mle as well
-# try emperic and logistic
-# maybe check trial design .org
-
 result = crmsim(r1, r1, target, n, x0 =1, nsim = 1, mcohort = 1, restrict = TRUE,
        count = TRUE, method = "bayes", model = "empiric", intcpt = 3,
        scale = sqrt(1.34), seed = 1009)
+
 
