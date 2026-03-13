6218c77451077e5380bf34d8ead9a7ff5fb74a37
Darya-Niaki <120230954+Darya-Niaki@users.noreply.github.com>
2026-03-13 16:00:00 -0400

    Update Single-agent-CDP-one-trial-DIP.R

--- Single-agent-CDP-one-trial-DIP.R
+++ Single-agent-CDP-one-trial-DIP.R
@@ -1,11 +1,18 @@
+# p0: Vector of true DLT probabilities for each dose level.
+# target: Target DLT rate (θ), representing the desired toxicity level.
+# ssize: Total sample size for the trial.
+# n.stop: Early stopping rule; the trial stops if the number of patients treated at a single dose level exceeds n.stop.
+# cl: Safety stopping cutoff used to terminate the trial when excessive toxicity is observed.
+# method: Specifies the DIP approach used to estimate toxicity probabilities. Options are "mean" or "mode".
+# N: Maximum effective sample size (ESS) for the prior distribution.
 
-cdp_DIP<-function(p0,target,ssize,n.stop,cl){
+
+cdp_DIP<-function(p0,target,ssize,n.stop,cl = 0.95,method,N){
     ndose = length(p0);   #number of dose levels
   y=n=dose.select=numeric(ndose);  #number of DLTs at each dose level
     stop=0; #indicate if trial stops early
     curr = 1
     i=1
-    N = 20 #stopping rule for each dose level
     while(i <= ssize){
       
       a0 = 1 + target*(N - n)
@@ -18,12 +25,6 @@ cdp_DIP<-function(p0,target,ssize,n.stop,cl){
       y[curr] = y[curr] + rbinom(1,1,p0[curr]);
     
       n[curr] = n[curr] + 1;
-      #a0 = target*(ssize - n)
-      
-      #b0 = (1-target)*(ssize - n)
-      #setting the stopping rule for the ssize to be 15 instead of the ssize
-      #setting the mode to be centered at the p0
-      
       tried=which(n>0)
     
       safety=ifelse(n[1]>1,1 - pbeta(target, y[1] + a0, n[1] - y[1] + b0),0)
@@ -32,9 +33,11 @@ cdp_DIP<-function(p0,target,ssize,n.stop,cl){
         stop=1
         break
       }
-    
-  u=(y[tried]+a0[tried] -1 )/(n[tried]+a0[tried]+b0[tried] -2)
-    
+      if(method=="mode"){
+          u=(y[tried]+a0[tried] -1 )/(n[tried]+a0[tried]+b0[tried] -2)
+      }else if(method=="mode"){
+          u=(y[tried]+a0[tried])/(n[tried]+a0[tried]+b0[tried])
+      }
       pipost=pava(u,w=n)
       lossvec=abs(pipost-target)  
       T=lossvec==min(lossvec)
@@ -71,10 +74,6 @@ cdp_DIP<-function(p0,target,ssize,n.stop,cl){
     return(list(dose.select=dose.select,tox.data=y,pt.allocation=n,stop=stop,a0 = a0,b0 = b0))
   }
 
+# cdp_DIP(p0,target,ssize,n.stop,cl,method = "mean",N = 20)
+# cdp_DIP(p0,target,ssize,n.stop,cl,method = "mode",N = 30)
 
-# p0 = c(0.3,	0.36,	0.42,	0.45,	0.46)
-# target = 0.25
-# ssize = 30
-# n.stop = 31
-# cl = 0.95
-# cdp_DIP(p0,target,ssize,n.stop,cl)
